69b816f4f3c56e477eb4e2320993622e19b31678
dmanikym <dmanikyam@traveltripper.com>
2016-10-04 13:07:21 +0530

    Shift to TT.io

---
+++ js/rt3-filters.js
@@ -0,0 +1,17 @@
+angular.module('rezTrip')
+  .filter('isArray', [function() {
+    return function(input) {
+      return angular.isArray(input);
+    };
+  }])
+.filter('formatpolicydescription', function() {
+    return function(text) {
+      return  text ? String(text).replace('["', '<p>').replace('"]', '</p>').replace(',',' ') : '';
+    }
+  }
+)
+.filter('ampersand', function(){
+    return function(input){
+        return input ? input.replace(/&amp;/, '&') : '';
+    }
+});
--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,26 @@ $(".campaign-item").height(maxHeight);
           // itemsMobile : false
      
       });
-     
+	  
+	  $(".filter").on("click", function () {
+    var $this = $(this);
+    // if we click the active tab, do nothing
+    if (!$this.hasClass("active")) {
+      $(".filter").removeClass("active");
+      $this.addClass("active"); // set the active tab
+      var $filter = $this.data("rel"); // get the data-rel value from selected tab and set as filter
+      $filter == 'all' ? // if we select "view all", return to initial settings and show all
+        $(".fancybox").attr("data-fancybox-group", "gallery").not(":visible").fadeIn() 
+        : // otherwise
+        $(".fancybox").fadeOut(0).filter(function () { 
+          return $(this).data("filter") == $filter; // set data-filter value as the data-rel value of selected tab
+        }).attr("data-fancybox-group", $filter).fadeIn(1000); // set data-fancybox-group and show filtered elements
+    } // if
+  }); // on
+	 
+     $('#custom-easing').lightGallery({
+		cssEasing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+	});
     });
 
 $(window).on('load scroll resize', function () {
@@ -119,6 +138,58 @@ $('#homeCarousel,#specialsCarousel').carousel({
   interval: 7000,
   cycle: true
 }); 
+
+
+  // title equal heights
+  
+$(window).on('load resize', function () {
+var maxHeight = 0;
+
+
+	var mwidth = $(window).width();
+if(mwidth > 768){
+// Home specials title equal heights
+
+$(".home-specials-item h3").each(function(){
+   if ($(this).height() > maxHeight) { 
+     maxHeight = $(this).height(); 
+ }
+});
+
+$(".home-specials-item h3").height(maxHeight);
+
+// Home specials description equal heights
+$(".home-specials-item h3").each(function(){
+   if ($(this).height() > maxHeight) { 
+     maxHeight = $(this).height(); 
+ }
+});
+
+$(".home-specials-item p").height(maxHeight);
+
+// Rooms title equal heights
+
+$(".rooms-item h2").each(function(){
+   if ($(this).height() > maxHeight) { 
+     maxHeight = $(this).height(); 
+ }
+});
+
+$(".rooms-item h2").height(maxHeight);
+
+// Rooms title equal heights
+
+$(".rooms-item p").each(function(){
+   if ($(this).height() > maxHeight) { 
+     maxHeight = $(this).height(); 
+ }
+});
+
+$(".rooms-item p").height(maxHeight);
+
+}
+
+});
 
 
 
