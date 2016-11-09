17bdfc2f81b4d147c1ec2f7c12cd4d4989562228
dmanikym <dmanikyam@traveltripper.com>
2016-11-09 20:17:04 +0530

    Nav final edits

--- js/scripts.js
+++ js/scripts.js
@@ -276,14 +276,17 @@ $(document).ready(function() {
 
 
 $(window).load(function(){ 
-  if( $(window).width() > 1254 ) {
-     jQuery('ul.nav li.dropdown a.dropdown-toggle').addClass('disable');
-     jQuery('ul.nav li.dropdown a.dropdown-toggle').removeAttr('data-toggle');
+  if( $(window).width() > 768 ) {
+     
+     jQuery('ul.nav li.dropdown a').attr('data-toggle','disable');
    //Add Hover effect to menus
         jQuery('ul.nav li.dropdown').hover(function() {
           jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
         }, function() {
           jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
         });
+  }
+  else{
+	   //jQuery('ul.nav li.dropdown a.dropdown-toggle').removeAttr('data-toggle');
   }
 });
